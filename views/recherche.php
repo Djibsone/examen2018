f28047e5195f122040aab7ad3888fe44602c9933
Djibril <djibriltamou@gmail.com>
2023-07-05 22:29:13 +0100

    modif

--- views/recherche.php
+++ views/recherche.php
@@ -6,7 +6,13 @@
 
   <div class="containe">
     <!-- menu start -->
-    <?php include './menu.php' ?>
+    <div class="sidebar card">
+        <h2>MENU GENERAL</h2>
+        <ul>
+            <li><span>1.</span> <a href="../">Inscription</a></li>
+            <li><span>2.</span> <a href="./recherche.php" class="list">Liste des candidats</a></li>
+        </ul>
+    </div>
     <!-- menu end -->
 
     <div class="content">
